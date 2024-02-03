335b5dd40e690962541c3d9b41934af582bd20fc
Ahmed Al Abassy <z5312664@ad.unsw.edu.au>
2024-02-03 11:17:56 +1100

    project init

---
+++ greenliving_app/src/constants/index.js
@@ -0,0 +1,22 @@
+import {} from "../assets";
+
+//Web constants, such as paragraphs, titles, images, etc
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
